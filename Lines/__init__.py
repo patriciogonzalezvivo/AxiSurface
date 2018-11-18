c9549dc0b28736e67fb9a950d0f421cc2c3af75a
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2018-11-18 05:05:24 -0800

    adding functions

--- Lines/__init__.py
+++ Lines/__init__.py
@@ -4,5 +4,5 @@
 from Surface import Surface
 
 # drawing primitives
-from primitives import dot, line
 from text import text
+from primitives import dot, line, arc, circle, rect, polyline, path, smoothPath
--- Lines/math_func.py
+++ Lines/math_func.py
@@ -10,6 +10,7 @@ import math
 import numpy as np
 
 # Basic trigonometry
+# -----------------------------------------------------------------
 
 def normalize(v, tolerance=0.00001):
     mag2 = sum(n * n for n in v)
@@ -18,23 +19,15 @@ def normalize(v, tolerance=0.00001):
         v = tuple(n / mag for n in v)
     return np.array(v)
 
+
 def perpendicular(A, B):
     ab = np.array(B) - np.array(A)
     dir_ab = normalize(ab)
     return np.array([-dir_ab[1], dir_ab[0]])
 
+
 # Convertions
-
-def remap(value, leftMin, leftMax, rightMin, rightMax):
-    # Figure out how 'wide' each range is
-    leftSpan = leftMax - leftMin
-    rightSpan = rightMax - rightMin
-
-    # Convert the left range into a 0-1 range (float)
-    valueScaled = float(value - leftMin) / float(leftSpan)
-
-    # Convert the 0-1 range into a value in the right range.
-    return rightMin + (valueScaled * rightSpan)
+# -----------------------------------------------------------------
 
 def polar2xy(center_x, center_y, angle, radius):
     a = math.radians(angle)
